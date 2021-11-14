1759f328236e6f8ba9558edbf1904cbf9dc5f128
Bert <bert@cavrn.us>
2021-11-14 08:40:34 -0800

    trophy creation working

--- Assets/Editor/CreateTrophyWindow.cs
+++ Assets/Editor/CreateTrophyWindow.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Assets.GameModel;
+using UnityEditor;
 using UnityEngine;
 
-public class CreateTrophyWindow : MonoBehaviour
+public class CreateTrophyWindow : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private static CreateTrophyWindow window;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[MenuItem("Company Man/Create Trophy")]
+
+	static void Init()
+	{
+		window = (CreateTrophyWindow)EditorWindow.GetWindow(typeof(CreateTrophyWindow));
+		window.data = AssetDatabase.LoadAssetAtPath<GameData>("Assets/Data/GameData.asset");
+		window.Show();
+	}
+
+	private GameData data;
+	private string trophyName;
+	private NpcPicker npcPicker = new NpcPicker();
+
+	void OnGUI()
+	{
+		trophyName = EditorGUILayout.TextField("Name:", trophyName);
+
+		npcPicker.DrawNpcDropdown(data);
+
+		if (GUILayout.Button("Create!"))
+		{
+			CreateInteraction();
+
+			window.Close();
+		}
+	}
+
+	private void CreateInteraction()
+	{
+		Trophy trophy = ScriptableObject.CreateInstance<Trophy>();
+		trophy.Name = trophyName;
+		trophy.Id = trophyName + npcPicker.NpcId;
+
+		var foundNpc = data.FindNpc(npcPicker.NpcId);
+
+		if (foundNpc == null)
+			return;
+
+
+		foundNpc.Item2.Trophies.Add(trophy);
+		EditorUtility.SetDirty(foundNpc.Item2);
+
+		AssetDatabase.CreateAsset(trophy, $"Assets/Data/{foundNpc.Item1.Id}/{foundNpc.Item2.Id}/Trophies/{trophy.Name}.asset");
+		AssetDatabase.SaveAssets();
+	}
+
 }
